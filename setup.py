deb196849ab31e0648f5a0096f6037ae0bdce0c6
Sanjaysai456 <polojisanjaysai9849@gmail.com>
2026-01-11 12:10:48 +0530

    project structure set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Poloji Sanjay Sai",
+    author_email="sanjaysai.poloji23@st.niituniversity.in",
+    packages=find_packages()
+) 
